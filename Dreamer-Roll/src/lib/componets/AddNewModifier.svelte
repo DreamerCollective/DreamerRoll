d815940b731455b4a73ca6a29a2e006ca22861ee
Zalron <davidrlawson@optusnet.com.au>
2023-06-15 16:29:14 +1000

    added new components AddNewDice.svelte, AddNewModifier.svelte These will handle the adding dice and modifier functionalities in a roll

---
+++ Dreamer-Roll/src/lib/componets/AddNewModifier.svelte
@@ -0,0 +1,14 @@
+<script>
+
+</script>
+
+<div class="relative block w-90% rounded-lg border-2 border-gray-300 p-1 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+  <form method="POST" action="?/UpdateDiceFacesRecord">
+    <div class="text-sm font-semibold leading-6 text-white">New Modifier</div>
+    <input type="text" name="DiceNames" id="DieNames" class="m-2 mx-20 block rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="New Modifier Name">
+    <input type="number" name="DiceFaces" id="DiceFaces" class="m-2 mx-20 block rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="New Modifier Number">
+  </form>
+  <button type="button" class="m-3.5 rounded-md bg-indigo-600 py-1 px-4 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+    <span class="text-sm font-semibold leading-6 text-white">Add Die</span>
+  </button>
+</div>

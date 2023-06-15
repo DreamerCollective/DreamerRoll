a867adc4006c6378305ff7faa40b9adae0297e71
Zalron <davidrlawson@optusnet.com.au>
2023-06-15 21:57:37 +1000

    updating Roll.svelte component to bring all of the updates together updated layouts to make it easier to distinguish between dice and modifier components of the roll

--- Dreamer-Roll/src/lib/componets/Roll.svelte
+++ Dreamer-Roll/src/lib/componets/Roll.svelte
@@ -4,26 +4,32 @@
   import DiceComboBox from "$lib/componets/DiceComboBox.svelte";
   import ModifierComboBox from "$lib/componets/ModifierComboBox.svelte";
   import { pb } from "$lib/pocketbase.js";
+  import AddNewModifier from "$lib/componets/AddNewModifier.svelte";
+  import AddNewDice from "$lib/componets/AddNewDice.svelte";
   export let record
 
 </script>
-
+{@debug record}
 <div class="mt-6 my-1 mx-0.5 divide-y divide-gray-500/25space-y-2 py-2 sm:py-12 lg:py-16">
   <div class="relative block rounded-lg border-2 border-gray-300 p-1 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-    <div class="flex-auto" >
+
+    <div class="relative block w-90% rounded-lg border-2 border-gray-300 p-1 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+      <div class="text-sm font-semibold leading-6 text-white">Dice</div>
       {#each record.rolldies as recordDice }
-        {}
+        {@debug recordDice}
         <Dice record="{recordDice}"/>
       {/each}
-
       <DiceComboBox />
-    </div>
-    <div class="flex-auto">
+      <AddNewDice />
+      </div>
+    <div class="relative block w-90% rounded-lg border-2 border-gray-300 p-1 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+      <div class="text-sm font-semibold leading-6 text-white">Modifiers</div>
       {#each record.rollmodifiers as recordModifier }
+        {@debug recordModifier}
         <Modifier record="{recordModifier}"/>
       {/each}
-
       <ModifierComboBox />
+      <AddNewModifier />
     </div>
     <div class="flex">
       <button type="button" class="flex m-2 rounded-md bg-indigo-600 py-2.5 px-3.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">

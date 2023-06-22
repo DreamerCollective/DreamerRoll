60438d25a196cbadd890ef2e74869a636db3c5b1
Zalron <davidrlawson@optusnet.com.au>
2023-06-22 18:31:15 +1000

    Got the comboboxes to work

--- Dreamer-Roll/src/lib/componets/ModifierComboBox.svelte
+++ Dreamer-Roll/src/lib/componets/ModifierComboBox.svelte
@@ -25,11 +25,11 @@
         <path fill-rule="evenodd" d="M10 3a.75.75 0 01.55.24l3.25 3.5a.75.75 0 11-1.1 1.02L10 4.852 7.3 7.76a.75.75 0 01-1.1-1.02l3.25-3.5A.75.75 0 0110 3zm-3.76 9.2a.75.75 0 011.06.04l2.7 2.908 2.7-2.908a.75.75 0 111.1 1.02l-3.25 3.5a.75.75 0 01-1.1 0l-3.25-3.5a.75.75 0 01.04-1.06z" clip-rule="evenodd" />
       </svg>
     </button>
-    {#each allModifierRecords as allModifierRecord}
-      {@debug allModifierRecord}
-      <ModifierComboOption allModifierRecord = "{allModifierRecord}" />
-    {/each}
   </div>
+  {#each allModifierRecords as allModifierRecord}
+    {@debug allModifierRecord}
+    <ModifierComboOption allModifierRecord = "{allModifierRecord}" />
+  {/each}
 {/if}
 {#if !ModifierComboBoxButton}
   <label for="combobox" class="block text-sm font-medium leading-none text-gray-900">Add Modifier</label>
--- Dreamer-Roll/src/lib/componets/ModifierComboOption.svelte
+++ Dreamer-Roll/src/lib/componets/ModifierComboOption.svelte
@@ -3,9 +3,8 @@
 </script>
 
 
-<ul class="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm" id="options" role="listbox">
-  <li class="relative cursor-default select-none py-2 pl-8 pr-4 text-gray-900" id="option-0" role="option" tabindex="-1">
+<div class="z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+  <div class="relative pl-8 pr-4 text-gray-900">
     <span class="block truncate">Name {allModifierRecord.modifiername} Dice Faces {allModifierRecord.modifiernumber}</span>
-    <span class="absolute inset-y-0 left-0 flex items-center pl-1.5 text-indigo-600"></span>
-  </li>
-</ul>
+  </div>
+</div>

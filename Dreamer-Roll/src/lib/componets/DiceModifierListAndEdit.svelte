1b03b85f0e29e8398b60397851b33fc7ecffc280
Zalron <davidrlawson@optusnet.com.au>
2023-08-16 23:46:35 +1000

    renaming functions and variables in DiceModifierListAndEdit.svelte

--- Dreamer-Roll/src/lib/componets/DiceModifierListAndEdit.svelte
+++ Dreamer-Roll/src/lib/componets/DiceModifierListAndEdit.svelte
@@ -8,27 +8,27 @@
   export let modifierRecord
 
   let DiceModifierListComboBoxButton = true
-  function expandDiceModifierListComboBox()
+  function expandDiceModifierList()
   {
     DiceModifierListComboBoxButton = DiceModifierListComboBoxButton !== true;
   }
 
-  let DiceListComboBoxButton = true
-  function expandDiceListComboBox()
+  let DiceListExpandButton = true
+  function expandDiceList()
   {
-    DiceListComboBoxButton = DiceListComboBoxButton !== true;
+    DiceListExpandButton = DiceListExpandButton !== true;
   }
 
-  let ModifierListComboBoxButton = true
-  function expandModifierListComboBox()
+  let ModifierListExpandButton = true
+  function expandModifierList()
   {
-    ModifierListComboBoxButton = ModifierListComboBoxButton !== true;
+    ModifierListExpandButton = ModifierListExpandButton !== true;
   }
 
 </script>
 <div class="flex flex-col flex-wrap">
   <div class="relative block w-90% rounded-lg border-2 border-gray-300 p-1 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-    <button type="button" on:click={expandDiceModifierListComboBox} class="inline-flex w-full justify-center gap-x-1.5 rounded-md px-3 py-2 text-sm font-semibold text-white shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-500" id="menu-button" aria-expanded="true" aria-haspopup="true">
+    <button type="button" on:click={expandDiceModifierList} class="inline-flex w-full justify-center gap-x-1.5 rounded-md px-3 py-2 text-sm font-semibold text-white shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-500" id="menu-button" aria-expanded="true" aria-haspopup="true">
       Dice and Modifier List
       <svg class="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
         <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
@@ -36,13 +36,13 @@
     </button>
   {#if DiceModifierListComboBoxButton}
   <div class="relative block w-90% rounded-lg border-2 border-gray-300 p-1 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-    <button type="button" on:click={expandDiceListComboBox} class="inline-flex w-full justify-center gap-x-1.5 rounded-md px-3 py-2 text-sm font-semibold text-white shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-500" id="menu-button" aria-expanded="true" aria-haspopup="true">
+    <button type="button" on:click={expandDiceList} class="inline-flex w-full justify-center gap-x-1.5 rounded-md px-3 py-2 text-sm font-semibold text-white shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-500" id="menu-button" aria-expanded="true" aria-haspopup="true">
       Dice List
       <svg class="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
         <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
       </svg>
     </button>
-    {#if DiceListComboBoxButton}
+    {#if DiceListExpandButton}
     <div class="flex flex-row flex-wrap">
       {#each diceRecord as recordDice }
         <DiceList record="{recordDice}"/>
@@ -52,13 +52,13 @@
     {/if}
   </div>
   <div class="relative block w-90% rounded-lg border-2 border-gray-300 p-1 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-    <button type="button" on:click={expandModifierListComboBox} class="inline-flex w-full justify-center gap-x-1.5 rounded-md px-3 py-2 text-sm font-semibold text-white shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-500" id="menu-button" aria-expanded="true" aria-haspopup="true">
+    <button type="button" on:click={expandModifierList} class="inline-flex w-full justify-center gap-x-1.5 rounded-md px-3 py-2 text-sm font-semibold text-white shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-500" id="menu-button" aria-expanded="true" aria-haspopup="true">
       Modifier List
       <svg class="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
         <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
       </svg>
     </button>
-    {#if ModifierListComboBoxButton}
+    {#if ModifierListExpandButton}
     <div class="flex flex-row flex-wrap">
       {#each modifierRecord as recordModifier }
         <ModifierList record="{recordModifier}"/>

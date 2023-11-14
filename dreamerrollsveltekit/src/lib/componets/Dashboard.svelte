3d28a3043ba422126a1b15aec85501413059f5f6
Zalron <davidrlawson@optusnet.com.au>
2023-11-14 23:04:39 +1100

    Added breadcrumb

--- dreamerrollsveltekit/src/lib/componets/Dashboard.svelte
+++ dreamerrollsveltekit/src/lib/componets/Dashboard.svelte
@@ -13,8 +13,37 @@
 
 
 </script>
+<nav class="flex border-b border-gray-200 bg-white" aria-label="Breadcrumb">
+    <ol role="list" class="flex px-4 sm:px-48 md:px-48 lg:px-48">
+        <li class="flex">
+            <div class="flex items-center">
+                <a href="/" class="text-gray-400 hover:text-gray-500">
+                    <svg class="h-5 w-5 flex-shrink-0" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                        <path fill-rule="evenodd" d="M9.293 2.293a1 1 0 011.414 0l7 7A1 1 0 0117 11h-1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-3a1 1 0 00-1-1H9a1 1 0 00-1 1v3a1 1 0 01-1 1H5a1 1 0 01-1-1v-6H3a1 1 0 01-.707-1.707l7-7z" clip-rule="evenodd" />
+                    </svg>
+                    <span class="sr-only">Home</span>
+                </a>
+            </div>
+        </li>
+        <li class="flex">
+            <div class="flex items-center">
+                <svg class="h-full w-6 flex-shrink-0 text-gray-200" viewBox="0 0 24 44" preserveAspectRatio="none" fill="currentColor" aria-hidden="true">
+                    <path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
+                </svg>
+                <a href="/" class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700" aria-current="page">Projects</a>
+            </div>
+        </li>
+        <li class="flex">
+            <div class="flex items-center">
+                <svg class="h-full w-6 flex-shrink-0 text-gray-200" viewBox="0 0 24 44" preserveAspectRatio="none" fill="currentColor" aria-hidden="true">
+                    <path d="M.293 0l22 22-22 22h1.414l22-22-22-22H.293z" />
+                </svg>
+                <a href="/" class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700" aria-current="page">Project Nero</a>
+            </div>
+        </li>
+    </ol>
+</nav>
 <div>
-
     <!-- Off-canvas menu for mobile, show/hide based on off-canvas menu state. -->
     <div id="user-menu-button-mobile" class="relative z-40 md:hidden" role="dialog" aria-modal="true">
         <!--
@@ -185,7 +214,7 @@
                         <svg class="text-gray-400 group-hover:text-gray-300 mr-4 flex-shrink-0 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5" />
                         </svg>
-                        Game Board
+                        Item Sheets
                     </a>
 
                     <a href="/" class="text-gray-300 hover:bg-gray-700 hover:text-white group flex items-center px-2 py-2 text-base font-medium rounded-md">
